2fbc7feef4ca1e7e76d881ec63ef819151fec809
shakib-shajid <shakibalshajid1682@gmail.com>
2024-11-29 22:10:11 +0600

    brand added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Banner from "@/components/Banner";
+import Brand from "@/components/Brand";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <About />
+      <Brand />
     </>
   );
 }
